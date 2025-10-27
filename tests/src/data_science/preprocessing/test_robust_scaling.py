0a9096f93e699552c607a94fe21cb3051c0c0671
iannickgagnon <iannick.gagnon@etsmtl.ca>
2025-10-27 17:41:20 -0400

    test: Add unit tests for robust scaling function

---
+++ tests/src/data_science/preprocessing/test_robust_scaling.py
@@ -0,0 +1,44 @@
+import pytest
+
+from src.data_science.preprocessing.robust_scaling import robust_scale
+
+
+def test_valid_input_success():
+  """
+  Tests that the function returns the correct for a valid input.
+
+  The input vector also contains a negative value.
+  """
+
+  # Get result
+  input_value = [1, 2, 3, 4, 5]
+  obtained_result = robust_scale(input_value)
+
+  # Reference
+  expected_result = [-2/3, -1/3, 0.0, 1/3, 2/3]
+
+  assert pytest.approx(obtained_result) == expected_result
+
+  
+def test_raises_wrong_input_type():
+  """
+  Tests that the proper error is raised when the input is of the incorrect type.
+  """
+  with pytest.raises(TypeError):
+    robust_scale(None)
+
+
+def test_raises_empty_list():
+  """
+  Tests that the proper error is raised when the input list is empty.
+  """
+  with pytest.raises(ValueError):
+    robust_scale([])
+
+
+def test_raises_list_with_non_numbers():
+  """
+  Tests that the proper error is raised when the input list contains non-numbers.
+  """
+  with pytest.raises(TypeError):
+    robust_scale([1, 1.0, None])
